e42d9066f55e29adf666b9dfa49e02c39ae3fb56
Folayemi12 <Folayemia0612@students.bowiestate.edu>
2023-11-03 16:49:12 +0100

    Problem2

---
+++ Problem2.py
@@ -0,0 +1,10 @@
+# calculating the area of a circle
+# importing math module
+import math
+radius= float(input("Enter the value for radius:"))
+# math.pi is a mathematical function to ascertain the value for pi
+pi = input(math.pi)
+# formula-Area_of_a_circle=pi*radius**2
+Area_of_a_circle = (math.pi*radius**2)
+print("Area_of_a_circle=", Area_of_a_circle)
+
