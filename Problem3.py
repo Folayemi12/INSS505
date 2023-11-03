e63863864698341741197dac9648023af4e6b173
Folayemi12 <Folayemia0612@students.bowiestate.edu>
2023-11-03 16:46:00 +0100

    Problem3

---
+++ Problem3.py
@@ -0,0 +1,14 @@
+"""
+calculating the sum and average from five users
+(NB) the total number of users is=5
+"""
+input1= float(input('Enter the value for input1:'))
+input2= float(input('Enter the value for input2:'))
+input3= float(input('Enter the value for input3:'))
+input4= float(input('Enter the value of input4:'))
+input5= float(input('Enter the value of input5:'))
+addition= input1+input2+input3+input4+input5
+average= ((input1+input2+input3+input4+input5)/5)
+print("sum=",addition)
+# Formula for average, average=sum/total number
+print("average=",average)
